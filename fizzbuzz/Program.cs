eecbfe935251853eae518a180bd776d6d5fd208c
PrimerFender <cspargo@gmail.com>
2021-03-12 08:49:21 -0800

    Upload fizzbuzz

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.Write(i);
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    Console.Write(" - ");
+                    if (i % 3 == 0) Console.Write("Fizz");
+                    if (i % 5 == 0) Console.Write("Buzz");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
